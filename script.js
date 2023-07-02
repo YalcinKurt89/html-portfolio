3c0b903357ee70b922492e2a7dcccc61df83166e
Yalcin Kurt <yalcin.kurt89@gmail.com>
2023-07-02 21:10:43 +0200

    added a new text in home.html and script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function changeText(text) {
     var textField = document.getElementById('text-field');
     textField.textContent = text;
   }
+
+var myInfo = document.getElementById('myInfo');
+var text = document.createTextNode('This page is for testing purposes only. On the homepage www.w3schools.com I already have the courses of HTML and CSS mostly through and have acquired the basic knowledge. Now I deal with JavaScript and will try to apply any new experience on this website. For this I have cloned my repository via cmd and only need to code my portfolio via Visual Studio Code. Afterwards I open cmd and type git add when I have created a new file. After writing codes to that file, I type in cmd git commit -am with the message to add the changes to the cache, so to speak. And when I am done with everything, I enter git push again in cmd to get the changes made uploaded to my repository on Github and updated.');
+myInfo.appendChild(text);
